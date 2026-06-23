d0b0738dcb67a531931cd78060c9c30ca8e5d21f
whqtker <chosh2001@inha.edu>
2026-06-23 11:34:25 +0900

    feat: enhance CORS configuration and implement token management in auth store

--- frontend-admin/src/api/http.js
+++ frontend-admin/src/api/http.js
@@ -15,6 +15,12 @@ export const http = axios.create({
   headers: { 'Content-Type': 'application/json' },
 })
 
+http.interceptors.request.use((config) => {
+  const token = localStorage.getItem('accessToken')
+  if (token) config.headers.Authorization = `Bearer ${token}`
+  return config
+})
+
 http.interceptors.response.use(
   (response) => response,
   (error) => Promise.reject(toApiError(error)),
