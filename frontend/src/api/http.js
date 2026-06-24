f74fb0e32dd5e55a84ac1856774c712c2f714748
whqtker <chosh2001@inha.edu>
2026-06-24 15:47:52 +0900

    fix: persist frontend auth token

--- frontend/src/api/http.js
+++ frontend/src/api/http.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getAccessToken } from './authToken.js'
 
 function toApiError(error) {
   if (!axios.isAxiosError(error)) return error
@@ -12,6 +13,14 @@ export const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
+})
+
+http.interceptors.request.use((config) => {
+  const token = getAccessToken()
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
 })
 
 http.interceptors.response.use(
