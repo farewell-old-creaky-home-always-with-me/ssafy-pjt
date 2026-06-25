572949ee1d361dd6be774daf266a277a48236756
whqtker <chosh2001@inha.edu>
2026-06-25 18:08:17 +0900

    style(infra): format terraform network config

--- infra/network.tf
+++ infra/network.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
-  tags = { Name = "${var.project_name}-vpc" }
+  tags                 = { Name = "${var.project_name}-vpc" }
 }
 
 resource "aws_internet_gateway" "main" {
@@ -15,21 +15,21 @@ resource "aws_subnet" "public" {
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "${var.aws_region}a"
   map_public_ip_on_launch = true
-  tags = { Name = "${var.project_name}-public" }
+  tags                    = { Name = "${var.project_name}-public" }
 }
 
 resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.10.0/24"
   availability_zone = "${var.aws_region}a"
-  tags = { Name = "${var.project_name}-private-a" }
+  tags              = { Name = "${var.project_name}-private-a" }
 }
 
 resource "aws_subnet" "private_b" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.11.0/24"
   availability_zone = "${var.aws_region}b"
-  tags = { Name = "${var.project_name}-private-b" }
+  tags              = { Name = "${var.project_name}-private-b" }
 }
 
 resource "aws_route_table" "public" {
